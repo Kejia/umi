b64fc9177cbe03551b1cf2237101047dc4181297
Daniel Wu <daniel.wu@cenx.com>
2016-04-02 23:25:20 -0400

    formatting

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject umi "0.1.2-SNAPSHOT"
   :description "a semantic web reasoning system"
-  :license {:name "GNU General Public License" :url "http://www.gnu.org/licenses/"}
+  :license {:name "GNU General Public License"
+            :url "http://www.gnu.org/licenses/"}
   :dependencies [[com.hermit-reasoner/org.semanticweb.hermit "1.3.8.4"]
                  [net.sourceforge.owlapi/owlapi-distribution "3.5.0"]
                  [org.clojure/clojure "1.7.0"]])
