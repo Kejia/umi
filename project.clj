1dd90aecb5fb4f5a62175c0747484d848867d426
Daniel Wu <daniel.wu@cenx.com>
2016-04-05 09:32:04 -0400

    adding url

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject umi "0.1.2-SNAPSHOT"
   :description "a semantic web reasoning lib"
+  :url "http://github.com/discretewater/umi"
   :license {:name "GNU General Public License"
             :url "http://www.gnu.org/licenses/"}
   :dependencies [[com.hermit-reasoner/org.semanticweb.hermit "1.3.8.4"]
